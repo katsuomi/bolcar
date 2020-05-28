14e7daead6f357e1a947f1eea1e9ba55d6047eb2
kotaro <ktrst0622@gmail.com>
2020-05-28 19:52:44 +0900

    add messages

--- db/migrate/20200526120109_remove_columnfrom_reservations.rb
+++ db/migrate/20200526120109_remove_columnfrom_reservations.rb
@@ -1,7 +1,7 @@
 class RemoveColumnfromReservations < ActiveRecord::Migration[5.2]
   def change
-    remove_column :reservations, :service_name
-    remove_column :reservations, :service_id
-    remove_column :reservations, :service_pwd
+    remove_column :reservations, :service_name, :string
+    remove_column :reservations, :servise_id, :string
+    remove_column :reservations, :servise_pwd, :string
   end
 end
