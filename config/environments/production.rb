d21f973e3584ac7485561dc5452917d057d48575
kotaro <ktrst0622@gmail.com>
2020-06-21 19:37:47 +0900

    introduce https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
 
-  config.action_mailer.default_url_options = { host: 'www.bolcar-test.xyz', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://www.bolcar-test.xyz' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
